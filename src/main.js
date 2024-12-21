a0e0c842eb4e692248e326b155a5e7e7a40e036a
Qleware <santosh.k@qleware.com>
2024-12-21 16:29:07 +0530

    initial content basing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 
 // Vuetify
-// import 'vuetify/styles'
-// import '@/styles/main.scss'
+import 'vuetify/styles'
+import '@/styles/main.scss'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
